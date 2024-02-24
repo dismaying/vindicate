dcdffd7cc876f9a6bd18d4c68e1e5f5482b04253
dismaying <149173124+dismaying@users.noreply.github.com>
2024-02-24 03:23:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -153,5 +153,5 @@ if __name__ == "__main__":
     banner()
     token = input(f"{CYAN}Enter User Token: {Style.RESET_ALL}").strip()
     client = Vindicate()
-    client.run(token, bot=False)
+    client.run(token)
     clear()
